5f43bf5d21d37fe021483f58569db35780284fd7
andres robles <andresinge07@gmail.com>
2025-05-16 16:57:29 -0500

    se empieza con la ui/ux  pantalla principal del juego

---
+++ src/views/game/components/FormComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <section
+    v-if="!gameReady"
+    class="w-fit py-4 px-2 text-white rounded-md flex gap-2"
+  >
+    <input
+      v-model="playerName"
+      type="text"
+      placeholder="Nombre"
+      class="px-4 py-2 rounded-md border border-white focus:outline-none focus:ring-0"
+    />
+    <input
+      v-model="gameCode"
+      type="text"
+      placeholder="Código de Juego"
+      class="px-4 py-2 rounded-md border border-white focus:outline-none focus:ring-0"
+      :disabled="joined"
+    />
+  </section>
+  <section class="flex gap-4 justify-center font-extrabold">
+    <button
+      class="bg-green-600 px-4 py-2 rounded-md hover:bg-green-700 disabled:opacity-50 transition duration-300 ease-in-out cursor-pointer"
+      @click="createGame"
+      :disabled="joined || !playerName"
+    >
+      Crear partida
+    </button>
+    <button
+      class="bg-blue-600 px-4 py-2 rounded-md hover:bg-blue-700 disabled:opacity-50 transition duration-300 ease-in-out cursor-pointer"
+    >
+      Unirse
+    </button>
+  </section>
+</template>
